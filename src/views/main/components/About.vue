20f430ddba3e6ae2e7c965bb036bfa7810a02d60
eunshinNoh <nes0410@gmail.com>
2022-09-10 17:20:37 -0600

    refs #4 main page divided to components

---
+++ src/views/main/components/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- About -->
+  <div>
+    <v-row>
+      <v-col class="ma-0 pa-0">
+        <v-card flat tile color="myGrey">
+          <v-card-title class="pt-16 pl-16 pb-8 myBlack--text text-h5">
+            Image: Lorem Ipsum has been the industry's standard dummy.
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-card flat tile color="myGrey">
+        <v-layout class="px-16 pb-16">
+          <v-img contain src="@/assets/images/image.jpg"></v-img>
+        </v-layout>
+      </v-card>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  components: {},
+  data: () => ({}),
+  watch: {},
+  computed: {},
+  mounted() {},
+  beforeDestroy() {},
+  methods: {},
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/views/main/components/Introduction.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- Introduction -->
+  <v-row>
+    <v-col class="ma-0 pa-0">
+      <v-card min-height="30vh" flat tile color="myBlack">
+        <v-card-title class="ma-16 pa-16 white--text text-h4">
+          Title: Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </v-card-title>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Introduction",
+  components: {},
+  data: () => ({}),
+  watch: {},
+  computed: {},
+  mounted() {},
+  beforeDestroy() {},
+  methods: {},
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
