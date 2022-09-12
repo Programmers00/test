facd1f5db5b524fd7731897ed3862f51f5874291
eunshinNoh <nes0410@gmail.com>
2022-09-12 10:47:54 -0600

    refs #4 Navbar visibility

--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -4,12 +4,12 @@
       <v-icon x-large color="white"> mdi-bug </v-icon>
       <v-toolbar-title class="white--text text-h4">NOhBUG</v-toolbar-title>
       <v-spacer></v-spacer>
-      <div v-if="$vuetify.breakpoint.width >= 1240 ? true : false">
+      <div class="d-none d-lg-block">
         <v-btn v-for="link in links" :key="link" text color="white">
           {{ link }}
         </v-btn>
       </div>
-      <div v-else>
+      <div class="d-lg-none">
         <v-btn text color="myBlack">
           <v-icon x-large color="white"> mdi-hamburger </v-icon>
         </v-btn>
